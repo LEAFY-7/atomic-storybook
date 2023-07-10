5cd8be256b7182537a7c4a89fededc480809c13f
ohtaekwon <ohtk1119@gmail.com>
2023-07-11 01:52:25 +0900

    [ADD] Drop Div 설명 추가

--- src/stories/drop-div.stories.tsx
+++ src/stories/drop-div.stories.tsx
@@ -8,7 +8,7 @@ export default {
   argTypes: {
     size: {
       control: {
-        type: "radio",
+        type: "select",
       },
       options: ["xxs", "xs", "sm", "md", "lg", "xl", "xxl", "xxxl"],
     },
@@ -17,6 +17,7 @@ export default {
 
 const Template = (args) => {
   const { size } = args;
+  const { width, height, shadow } = dropSizeBox[size];
 
   let description = "";
   switch (size) {
@@ -50,17 +51,25 @@ const Template = (args) => {
   }
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <DropDiv {...args} />
-      <p>{description}</p>
-    </div>
+    <section>
+      <h1>물방울 모양의 Div 컴포넌트</h1>
+      <li>size Props에 따라 물방울의 크기가 달라집니다.</li>
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <DropDiv {...args} />
+
+        <td style={{ fontSize: "16px" }}>넓이 : {width} px</td>
+        <td style={{ fontSize: "16px" }}>높이 : {height} px</td>
+      </div>
+    </section>
   );
 };
 
@@ -68,4 +77,47 @@ export const waterDrop = Template.bind({});
 waterDrop.args = {
   size: "lg",
   children: "",
+};
+
+const dropSizeBox = {
+  xxs: {
+    width: 40,
+    height: 40,
+    shadow: 10,
+  },
+  xs: {
+    width: 70,
+    height: 70,
+    shadow: 12,
+  },
+  sm: {
+    width: 100,
+    height: 100,
+    shadow: 15,
+  },
+  md: {
+    width: 120,
+    height: 120,
+    shadow: 18,
+  },
+  lg: {
+    width: 150,
+    height: 150,
+    shadow: 20,
+  },
+  xl: {
+    width: 180,
+    height: 180,
+    shadow: 25,
+  },
+  xxl: {
+    width: 200,
+    height: 200,
+    shadow: 30,
+  },
+  xxxl: {
+    width: 350,
+    height: 350,
+    shadow: 30,
+  },
 };
